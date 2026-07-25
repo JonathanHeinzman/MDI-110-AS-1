6a76d486880d951f43fc5a5a4eb99cd87ee6a09b
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-07-25 13:32:25 -0600

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Jonathan Heinzman on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
